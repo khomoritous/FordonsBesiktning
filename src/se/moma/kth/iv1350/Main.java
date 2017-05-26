be579c7578fd188cf890ea8d568800cd4d443448
monde <monde@monde-VirtualBox>
2017-05-26 08:47:43 +0200

    Hårdkodad körning utan enhetstester.

--- src/se/moma/kth/iv1350/Main.java
+++ src/se/moma/kth/iv1350/Main.java
@@ -16,34 +16,37 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       GarageDoor garageDoor = new GarageDoor();
-       Garage garage = new Garage(garageDoor);
-       CustomerQueue customerQueue = new CustomerQueue();
-       customerQueue.customerTakeQueueNumber();
-       customerQueue.nextCustomer();
-       garage.openGarage();
-       garage.closeGarage();
-       
-       Vehicle vehicle = new Vehicle(10,new Inspection(100));
-       
-       Controller controller = new Controller(vehicle);
+
+           GarageDoor garageDoor = new GarageDoor();
+            Garage garage = new Garage(garageDoor);
+            CustomerQueue customerQueue = new CustomerQueue();
+            customerQueue.customerTakeQueueNumber();
+            customerQueue.nextCustomer();
+            garage.openGarage();
+            garage.closeGarage(); 
+
+            
       
-       System.out.println("Inspection cost for Vehicle: "+ controller.registerNumber(10));
-       
-       System.out.println("Receipt from transaction: "+ controller.pay(100,new CreditCardInformation()));
-       
-       System.out.println("Inspection for the vehicle: " + controller.inspectVehicle());
-       
-       controller.enterResultOfInspection("Pass");
-       
-       controller.printResult();
-       
-       garage.openGarage();
-       garage.closeGarage();
-       
-       
+            
+            Vehicle vehicle = new Vehicle(10,new Inspection(100));
+
+            Controller controller = new Controller(vehicle);
+
+            System.out.println("Inspection cost for Vehicle: "+ controller.registerNumber(10));
+
+            System.out.println("Receipt from transaction: "+ controller.pay(100,new CreditCardInformation()));
+
+            System.out.println("Inspection for the vehicle: " + controller.inspectVehicle());
+
+            controller.enterResultOfInspection("Pass");
+
+            controller.printResult();
+        
       
+            garage.openGarage();
+            garage.closeGarage();
        
+   
  
        
        
