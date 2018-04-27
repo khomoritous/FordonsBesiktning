0b185476be437bf130b55089bebbc406b1cd9847
Monde Mampe <monde.mampe@gmail.com>
2018-04-27 11:37:24 +0200

    städar upp GarageDoorTest.

--- test/se/moma/kth/iv1350/model/external/GarageDoorTest.java
+++ test/se/moma/kth/iv1350/model/external/GarageDoorTest.java
@@ -10,6 +10,7 @@ import java.io.PrintStream;
 import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
@@ -20,6 +21,16 @@ public class GarageDoorTest {
      
      ByteArrayOutputStream outContent = null;
      GarageDoor garageDoor = null;
+     String result = null;
+     String expResult = null;
+     
+    @Before
+    public void setUp() {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        garageDoor = new GarageDoor();
+        
+    }
      
     @After
     public void cleanUpStreams() {
@@ -27,30 +38,23 @@ public class GarageDoorTest {
         
 
     }
+    
     @Test
     public void testCloseDoor() {
         
-        outContent = new ByteArrayOutputStream();
-       
-        System.setOut(new PrintStream(outContent));
-        
-        garageDoor = new GarageDoor();
         garageDoor.closeDoor();
-        String expResult = "Closing garage door...";
-        String result = outContent.toString();
+        expResult = "Closing garage door...";
+        result = outContent.toString();
         assertTrue("Garage Door not closing",result.contains(expResult));
         
     }
 
     @Test
     public void testOpenDoor() {
-        outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
         
-        garageDoor = new GarageDoor();
         garageDoor.openDoor();
-        String expResult = "Opening garage door...";
-        String result = outContent.toString();
+        expResult = "Opening garage door...";
+        result = outContent.toString();
         assertTrue("Garage Door not opening",result.contains(expResult));
     }
     
