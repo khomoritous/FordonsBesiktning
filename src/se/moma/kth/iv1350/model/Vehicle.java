38edd0d45569785cfb1cb10469cdab0ec5587606
Monde Mampe <monde.mampe@gmail.com>
2018-05-14 03:55:14 +0200

    lägger till equals- och hashmetod i Vehicle för att senare kunna kolla rätt vehicle på registreringsnummer i controller-klassen via vehicleregistry-klassen i controller-klassen.

--- src/se/moma/kth/iv1350/model/Vehicle.java
+++ src/se/moma/kth/iv1350/model/Vehicle.java
@@ -61,6 +61,31 @@ public class Vehicle {
        inspection.setResultOfInspection(result);
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.vehicleRegistrationNumber;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vehicle other = (Vehicle) obj;
+        if (this.vehicleRegistrationNumber != other.vehicleRegistrationNumber) {
+            return false;
+        }
+        return true;
+    }
+
     
     
     
